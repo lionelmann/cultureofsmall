dc7011ce169bec7852bb42606852db8b6c882cc7
Lionel Mann <lionelmann@gmail.com>
2015-05-22 16:22:09 -0400

    edit

--- wordpress/wp-content/themes/small/index.php
+++ wordpress/wp-content/themes/small/index.php
@@ -1,13 +1,16 @@
 <html>
 <head>
     <link rel='stylesheet' href='<?php bloginfo('template_url' ); ?>/style.css' async='async' type='text/css' media='all' />
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
 <body>
 
 <section role="slider">
 
     <img src='<?php bloginfo('template_url' ); ?>/dist/images/placeholder.png'>
-    
+    <div style="position: absolute; bottom: 50; left: 50%;"><a href="https://www.facebook.com/cultureofsmall"><i style="color: rgb(55, 151, 219);" class="fa fa-facebook-square fa-2x"></i></a>
+    </div>
+   
 </section>
 
 </body>
