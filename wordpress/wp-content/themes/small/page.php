848c6be9eebab23297fee229fd1cc62a9fccc6c2
Lionel Mann <lionelmann@gmail.com>
2015-05-25 22:26:51 -0400

    page template

--- wordpress/wp-content/themes/small/page.php
+++ wordpress/wp-content/themes/small/page.php
@@ -1,15 +1,34 @@
 <?php get_header();?>
-    <main>
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <article>
-                <h1><?php the_title(); ?></h1>
-                <?php the_content(); ?>
-            </article>
-            <aside>
-                <p>sidebar here</p>
-            </aside>
-        <?php endwhile; else : ?>
-            <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-        <?php endif; ?>
-    </main>
+
+<!-- Loop starts -->
+<?php if(have_posts()):while(have_posts()):the_post();?>
+
+<!-- Get post thumbnail url -->
+<?php
+    $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), "half-banner" );
+?>
+
+<!-- Banner with Title -->
+<section role="slider" style="background-image: url(<?php echo $src[0]; ?>); height: 400px; width: 100%; overflow: hidden;">
+    <header>
+        <hgroup>
+            <h1 class="page-headline"><?php the_title(); ?><h1>
+        </hgroup>
+    </header>
+</section>
+
+<!-- Main Article and Aside -->
+<main>
+    <article>
+        <?php the_content(); ?>
+    </article>
+    <aside>
+        <p>sidebar here</p>
+    </aside>
+</main>
+
+<?php endwhile; else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
 <?php get_footer();?>
--- wordpress/wp-content/themes/small/single.php
+++ wordpress/wp-content/themes/small/single.php
@@ -8,6 +8,7 @@
     $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), "half-banner" );
 ?>
 
+<!-- Banner with Title -->
 <section role="slider" style="background-image: url(<?php echo $src[0]; ?>); height: 400px; width: 100%; overflow: hidden;">
     <header>
         <hgroup>
@@ -16,19 +17,18 @@
     </header>
 </section>
 
+<!-- Main Article and Aside -->
+<main>
+    <article>
+        <?php the_content(); ?>
+    </article>
+    <aside>
+        <p>sidebar here</p>
+    </aside>
+</main>
 
-    <main>
-        
-            <article>
-                
-                <?php the_content(); ?>
-            </article>
-            <aside>
-                <p>sidebar here</p>
-            </aside>
-        
-    </main>
-    <?php endwhile; else : ?>
-            <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-        <?php endif; ?>
+<?php endwhile; else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
 <?php get_footer();?>
