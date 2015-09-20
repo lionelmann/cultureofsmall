76becc1a9f6ebfc04f2aeb22d0facaf229149180
Lionel Mann <lionelmann@gmail.com>
2015-09-20 14:37:47 -0400

    remove emoji

--- custom/themes/small/functions.php
+++ custom/themes/small/functions.php
@@ -18,7 +18,7 @@ require_once('includes/functions/register-sidebar.php');
 require_once('includes/functions/remove-header-meta.php');
 require_once('includes/functions/remove-menu-id.php');
 require_once('includes/functions/remove-image-dimensions.php');
-//require_once('includes/functions/remove-wp-version.php');
+require_once('includes/functions/remove-wp-version.php');
 //require_once('includes/shortcodes/button.php');
 //require_once('includes/functions/custom-cta-widget.php');
 
@@ -43,3 +43,27 @@ function crunchify_social_sharing_buttons($content) {
 	}
 };
 add_filter( 'the_content', 'crunchify_social_sharing_buttons');
+
+function disable_wp_emojicons() {
+
+  // all actions related to emojis
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+
+  // filter to remove TinyMCE emojis
+  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+}
+add_action( 'init', 'disable_wp_emojicons' );
+
+function disable_emojicons_tinymce( $plugins ) {
+  if ( is_array( $plugins ) ) {
+    return array_diff( $plugins, array( 'wpemoji' ) );
+  } else {
+    return array();
+  }
+}
