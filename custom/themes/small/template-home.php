6fd613968f709fcbba000b4051b1043055f08672
Lionel Mann <lionelmann@gmail.com>
2015-10-02 10:14:18 -0400

    add: sticky

--- custom/themes/small/template-home.php
+++ custom/themes/small/template-home.php
@@ -22,8 +22,10 @@ get_header();?>
     $paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
     $args = array(
         'post_type' => 'post',
-        'posts_per_page' => 9,
+        //'posts_per_page' => 9,
         'paged' =>  $paged,
+        'post__in'  => get_option( 'sticky_posts' ),
+        'ignore_sticky_posts' => 0,
 ); 
 
 $the_query = new WP_Query( $args ); ?>
@@ -40,7 +42,33 @@ $the_query = new WP_Query( $args ); ?>
                 </div>
             </div>
         </a>
-    <?php endwhile; endif; ?>
+    <?php endwhile; endif; wp_reset_postdata();?>
+
+
+<?php 
+    $paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
+    $args = array(
+        'post_type' => 'post',
+        'posts_per_page' => 8,
+        'paged' =>  $paged,
+        'post__not_in' => get_option( 'sticky_posts' ),
+        'ignore_sticky_posts' => 1,
+); 
+
+$the_query = new WP_Query( $args ); ?>
+
+    <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); 
+     
+        $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array(400, 500) ); ?>
+
+        <a href="<?php the_permalink(); ?>">
+            <div class="mix" style="background-image: url(<?php echo $src[0]; ?>);">
+                <div class="header">
+                    <span><?php the_title(); ?></span>
+                </div>
+            </div>
+        </a>
+    <?php endwhile; endif; wp_reset_postdata();?>
 </div>
 
 <?php pagination(); ?>
